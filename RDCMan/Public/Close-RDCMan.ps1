a55749b7ad010fae5cf54a0d18a952d4299164d3
Devyn Spencer <devynspencer@users.noreply.github.com>
2024-06-20 15:38:36 -0700

    Update Close-RDCMan.ps1
    Fix typos

--- RDCMan/Public/Close-RDCMan.ps1
+++ RDCMan/Public/Close-RDCMan.ps1
@@ -15,7 +15,7 @@ function Close-RDCMan {
 
         # Forcefully close remaining processes, if specified
         if ($Force -and (Get-Process -Id $Process.Id -ErrorAction SilentlyContinue)) {
-            Write-Verbose "Process [$($Process.Id)] with title $($Process.MainWindowTitle) did not close gracefully, stopping [$($Process.Id)"
+            Write-Verbose "Process [$($Process.Id)] with title $($Process.MainWindowTitle) did not close gracefully, stopping [$($Process.Id)]"
 
             if ($PSCmdlet.ShouldProcess("RDCMan process [$($Process.Id)] with title $($Process.MainWindowTitle)", 'Stop Process')) {
                 Stop-Process -Id $Process.Id -Force
